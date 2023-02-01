ad8cf403f16b9dba9c1ea45f2c837d711a27af7a
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-02-01 03:29:01 +0200

    Revert "hotfix / interpolation  deleted"
    This reverts commit 5f81eff4da4c2b03c35a8521be5fd357d43ef72e.

--- src/js/slider-config.js
+++ src/js/slider-config.js
@@ -62,14 +62,14 @@ function swiperSlider() {
                         slidesPerView: 1,
                         spaceBetween: 20,
                     },
-                    // when window width is >= 1024px
-                    1024: {
+                    // when window width is >= 768px
+                    768: {
                         slidesPerView: 2,
-                        spaceBetween: 40
+                        spaceBetween: 30
                     },
-                    // when window width is >= 1280px
-                    1280: {
-                        slidesPerView: 2,
+                    // when window width is >= 1200px
+                    1200: {
+                        slidesPerView: 3,
                         spaceBetween: 40
                     }
                 }
